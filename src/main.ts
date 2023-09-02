59d0e1722fea8e6eaa01ac6f57358ffd50c895c6
aquilaneo <aquilaneo@gmail.com>
2023-09-02 21:17:39 +0900

    Safari対応

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import { SendMediaChannel } from "./media-channel.ts";
 
 function initialize() {
     const connection = new Connection("", IceMode.VanillaIce);
+    let sdp = "";
 
     // Offerとして接続ボタン
     const connectOfferButton = document.getElementById("connect-offer-button");
     if (connectOfferButton) {
         connectOfferButton.onclick = async () => {
             const offerSdp = await connection.connectAsOffer();
-            await navigator.clipboard.writeText(offerSdp);
-            console.log("offer sdp copied")
+            try {
+                await navigator.clipboard.writeText(offerSdp);
+                console.log("offer sdp copied")
+            } catch {
+                // Safariはawaitの後のクリップボード書き込みはユーザーインタラクション起因じゃないと判定されエラーになるため別処理
+                sdp = offerSdp;
+            }
         };
     }
 
@@ -21,8 +27,13 @@ function initialize() {
         connectAnswerButton.onclick = async () => {
             const offerSdp = await navigator.clipboard.readText();
             const answerSdp = await connection.connectAsAnswer(offerSdp);
-            await navigator.clipboard.writeText(answerSdp);
-            console.log("answer sdp copied");
+            try {
+                await navigator.clipboard.writeText(answerSdp);
+                console.log("answer sdp copied");
+            } catch {
+                // Safariはawaitの後のクリップボード書き込みはユーザーインタラクション起因じゃないと判定されエラーになるため別処理
+                sdp = answerSdp;
+            }
         };
     }
 
@@ -35,11 +46,32 @@ function initialize() {
         };
     }
 
+    // SDPコピーボタン(Safari用)
+    const copySdpButton = document.getElementById("copy-sdp-button");
+    if (copySdpButton) {
+        copySdpButton.onclick = async () => {
+            await navigator.clipboard.writeText(sdp);
+        };
+    }
+
+    // SDP貼り付けボタン(Safari用)
+    const pasteSdpButton = document.getElementById("paste-sdp-button");
+    if (pasteSdpButton) {
+        pasteSdpButton.onclick = async () => {
+            sdp = await navigator.clipboard.readText();
+        };
+    }
+
     // カメラ有効化ボタン
     const enableCameraButton = document.getElementById("enable-camera-button");
     if (enableCameraButton) {
         enableCameraButton.onclick = async () => {
-            const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+            const mediaStream = await navigator.mediaDevices.getUserMedia({
+                video: {
+                    width: 1920, height: 1080,
+                    facingMode: { exact: "environment" }
+                }, audio: true
+            });
             const localVideoElem = document.getElementById("local-video") as HTMLVideoElement;
             localVideoElem.srcObject = mediaStream;
             localVideoElem.muted = true;
